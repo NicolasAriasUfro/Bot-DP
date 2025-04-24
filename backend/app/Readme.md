70134243e770fb2d6977689d7331e3010c86fe73
nico <n.arias10@ufromail.cl>
2025-04-24 18:34:03 -0400

    backend respondiendo al front

---
+++ backend/app/Readme.md
@@ -0,0 +1,10 @@
+first, setup your python envieoment. after continue with the following steps:
+
+
+``` bash
+
+
+pip install --no-cache-dir --upgrade -r requirements.txt
+
+fastapi run app/main.py --port 8000
+```
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,27 @@
 from typing import Union
+from pydantic import BaseModel
+from datetime import datetime
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
+class MessageInput(BaseModel):
+    message: str
+
+class MessageOutput(BaseModel):
+    message: str
+    timestamp: datetime
+
+# Permitir todos los orígenes (⚠️ Solo en desarrollo)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todos los dominios
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permitir todos los headers
+)
 
 @app.get("/")
 def read_root():
@@ -15,5 +34,19 @@ def read_item(item_id: int, q: Union[str, None] = None):
 
 @app.get("/chatbot")
 def reply():
+    #TODO: call to cedric main function
     chat_bot_response = "respuesta del chatbot"
     return chat_bot_response
+
+@app.post("/chatbot")
+def reply(input: MessageInput):
+    #TODO: call to cedric main function
+    print(input.message)
+    chat_bot_response = 'Ohh, "'+ input.message+'" es una interesante pregunta '
+    response = [
+        MessageOutput(
+            message=chat_bot_response,
+            timestamp=datetime.now()
+        )
+    ]
+    return response
