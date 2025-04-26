fa601637033d6bcb2f4df4087dd0dd38354fb0bd
admin-xpertia <info.xpertia@xpertia.cl>
2025-04-25 22:57:36 -0400

    feat[back]: add unit tests for NlpService classifier with weather, financial, and news intentions

---
+++ backend/src/test/test_classifier.py
@@ -0,0 +1,37 @@
+import unittest
+import sys
+import os
+
+# Add the parent directory to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from app.config.logger import Logger
+from app.service.nlp_service import NlpService
+
+
+class Test(unittest.TestCase):
+      def setUp(self):
+         self.nlp_service = NlpService()
+         self.logger = Logger()
+   
+      def test_classifier_with_weather_intention(self):
+         resp = self.nlp_service.classify_query('¿Cuál es el clima en Santiago?')         
+         self.logger.log(f'Classifier response: {resp}')
+         
+         self.assertTrue(resp == 'clima')
+         
+      def test_classifier_with_financial_intention(self):
+         resp = self.nlp_service.classify_query('¿Cuál es el valor del dolar?')         
+         self.logger.log(f'Classifier response: {resp}')
+         
+         self.assertTrue(resp == 'dolar')
+         
+      def test_classsifier_with_news_intention(self):
+         resp = self.nlp_service.classify_query('¿Cuáles son las últimas noticias?')         
+         self.logger.log(f'Classifier response: {resp}')
+         
+         self.assertTrue(resp == 'noticias')
+
+
+if __name__ == "__main__":
+   unittest.main()
